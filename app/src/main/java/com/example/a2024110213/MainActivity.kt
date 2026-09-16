e95062fd7fb01f3419c186a396209d6786a1d1da
sociyrbro <1565321242@qq.com>
2026-09-16 19:42:48 +0800

    完成多语言版本的Hello World并修改app图标

--- app/src/main/java/com/example/a2024110213/MainActivity.kt
+++ app/src/main/java/com/example/a2024110213/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.a2024110213
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -19,10 +20,24 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        // 根据系统当前语言更新对应国旗
+        updateFlag()
+
         val textView = findViewById<TextView>(R.id.textView)
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
-            textView.text = "I am clicked！"
+            textView.text = getString(R.string.hello_world)
         }
+    }
+
+    private fun updateFlag() {
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        val locale = resources.configuration.locales[0]
+        val flagRes = when (locale.language) {
+            "zh" -> R.drawable.china
+            "ko" -> R.drawable.hanguo
+            else -> R.drawable.us
+        }
+        imageView.setImageResource(flagRes)
     }
 }
